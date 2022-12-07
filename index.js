f7065aefb613b478ba8a7192e44d0b40c120c562
Taylor dos Santos Oliveira <taylor.oliveira@tcm.ba.gov.br>
2022-12-07 11:14:43 -0300

    edit routes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.render('index');
 });
 
